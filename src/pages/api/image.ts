9a4355584fa7589646acbc4227eea5693e6565ba
Gene Kogan <kogan.gene@gmail.com>
2023-04-18 12:19:02 -0700

    update

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -2,12 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import path from 'path';
 import fs from 'fs';
 
+const imagesPath = path.join(process.cwd(), 'public', 'highlights');
+const imageNames = fs.readdirSync(imagesPath).filter((fileName) => fileName.endsWith('.jpg'));
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const imagesPath = path.join(process.cwd(), 'public', 'highlights');
-  const imageNames = fs.readdirSync(imagesPath);
   const randomImageName = imageNames[Math.floor(Math.random() * imageNames.length)];
   const imagePath = path.join('/', 'highlights', randomImageName);
   // delay 1 second to simulate a slow API
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  //await new Promise((resolve) => setTimeout(resolve, 1000));  
   return res.status(200).json({ imagePath });
 };
